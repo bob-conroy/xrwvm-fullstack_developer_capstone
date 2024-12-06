47cfbd1ff245ef118a54d92efc8b586cdef51770
bob-conroy <bob.conroymba@gmail.com>
2024-12-06 17:08:01 -0500

    Updates made

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,14 +28,10 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
- # pylint: disable-next=line-too-long
-ALLOWED_HOSTS = ['localhost',
-                 'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-                ]
- # pylint: disable-next=line-too-long
-CSRF_TRUSTED_ORIGINS = [
-                        'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-                       ]
+# pylint: disable-next=line-too-long
+ALLOWED_HOSTS = ['localhost','https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+# pylint: disable-next=line-too-long
+CSRF_TRUSTED_ORIGINS = ['https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
