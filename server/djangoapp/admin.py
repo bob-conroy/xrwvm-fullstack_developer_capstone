a747e03b09f8bb1aaf3807e2cad766df1e68d67b
bob-conroy <bob.conroymba@gmail.com>
2024-12-06 16:05:42 -0500

    Updates made

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
 from .models import CarModel, CarMake
 
+
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 3
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type','year')
+    list_display = ('name', 'type', 'year')
     list_filter = ['year', 'type']
     search_fields = ['name', 'type', 'year']
-    
+
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ('name', 'description')
     list_filter = ['name']
     search_fields = ['name']
+
 
 # Register models here
 admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,7 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -11,6 +10,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
