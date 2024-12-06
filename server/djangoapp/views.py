2d3c1f24c7ee1775f51238f9a41a819bc47a892a
bob-conroy <bob.conroymba@gmail.com>
2024-12-06 14:44:30 -0500

    Updates made

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,4 @@
 # Uncomment the required imports before adding the code
-
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
@@ -15,12 +14,10 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
+from .restapis import get_request, post_review, analyze_review_sentiments
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
-
-# Create your views here.
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
