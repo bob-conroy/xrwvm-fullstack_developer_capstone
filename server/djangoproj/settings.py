dce8bda8f1dc3e171843d902f01281a97a0c7508
bob-conroy <bob.conroymba@gmail.com>
2024-12-06 16:52:15 -0500

    Updates made

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,11 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost',
-    'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+ALLOWED_HOSTS = 
+    ['localhost',
+     'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
-CSRF_TRUSTED_ORIGINS=[
-    'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+CSRF_TRUSTED_ORIGINS = 
+    [
+     'https://bobconroymba-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 
 REST_FRAMEWORK = {
@@ -96,22 +98,22 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-     'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-     'NAME':
-     'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-     'NAME':
-     'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-     'NAME':
-     'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+   'NAME':
+   'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+   'NAME':
+   'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+   'NAME':
+   'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+   'NAME':
+   'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
 
 
